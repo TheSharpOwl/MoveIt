adc221956ab8ea1024b2bd41c26b1d2064e2256c
TheSharpOwl <13377190+TheSharpOwl@users.noreply.github.com>
2023-01-23 02:00:04 +0300

    Finish initial solution with constants.

--- MoveIt.cpp
+++ MoveIt.cpp
@@ -5,10 +5,20 @@
 #include <conio.h>
 #include <stdio.h>
 
+/**
+ * Todo
+ * 1. add mouse movement detection
+ * 2. add another sound for beeping
+ * 3. add option for sound if break time is also over
+ */
 int main()
 {
 	auto const breakTimeMin = 5;
 	auto const workTimeMin = 25;
+
+	float continuousWorkTime = 0;
+
+	bool isBreak = false;
 	bool keyIsPressed = false;
 	auto start = std::chrono::steady_clock::now();
 
@@ -28,14 +38,25 @@ int main()
 		}
 
 
-
 		if(keyIsPressed)
 		{
 			auto now = std::chrono::steady_clock::now();
-			auto timePassed = std::chrono::duration_cast<std::chrono::minutes>(now - start).count();
+			auto timePassed = std::chrono::duration_cast<std::chrono::seconds>(now - start).count();
 			start = now;
-			std::cout << '\a';
 			std::cout << "Since last key stroke in minutes " << timePassed << "\n";
+
+			if (timePassed < breakTimeMin * 60)
+			{
+				continuousWorkTime += timePassed / 60.f;
+			}
+			else
+			{
+				continuousWorkTime = 0.f;
+			}
+
+			// if you work more than worktime and still using the computer, beep on every keystroke
+			if(continuousWorkTime >= workTimeMin)
+				std::cout << '\a'; // beep
 		}
 
 		keyIsPressed = false;
